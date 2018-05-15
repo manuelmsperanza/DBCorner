54660ae2218300577d9c6bf1f12046b561aad7e6
manuelmsperanza <manuel.m.speranza@gmail.com>
2018-05-15 20:44:09 +0200

    Fixed lib build path

--- OracleConn/src/main/java/net/dtdns/hoffnungland/db/corner/oracleconn/OrclConnectionManager.java
+++ OracleConn/src/main/java/net/dtdns/hoffnungland/db/corner/oracleconn/OrclConnectionManager.java
@@ -2,7 +2,6 @@ package net.dtdns.hoffnungland.db.corner.oracleconn;
 
 import java.sql.CallableStatement;
 import java.sql.Clob;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
