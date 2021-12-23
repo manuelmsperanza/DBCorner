09393d146e01267ddf8dd9f13944b62582a8999d
manuelmsperanza <manu.speranz@gmail.com>
2021-12-23 18:17:53 +0100

    H2DBConn first version

---
+++ H2DBConn/src/main/java/com/hoffnungland/db/corner/h2dbconn/App.java
@@ -0,0 +1,52 @@
+package com.hoffnungland.db.corner.h2dbconn;
+
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+	private static final Logger logger = LogManager.getLogger(App.class);
+	
+	public static void main( String[] args )
+    {
+    	Properties connectionProps = new Properties();
+		connectionProps.put("user", "h2dbuser");
+		connectionProps.put("password", "h2dbfilePasswd h2dbuserPasswd");
+
+		String dbms = "h2";
+		String dbName = "~/h2dbEnc;CIPHER=AES";
+
+		String urlString = "jdbc:" + dbms + ":" + dbName;
+		connectionProps.put("URL", urlString);
+		
+		H2ConnectionManager dbManager = null;
+		try {
+			dbManager = new H2ConnectionManager();
+			dbManager.connect(urlString, connectionProps);
+			
+			CallableStatement stmSet = dbManager.getCallableStatement("ddl/tables.sql").getStm();
+			stmSet.execute();
+			
+			CallableStatement stmCompact = dbManager.getCallableStm("SHUTDOWN COMPACT");
+			stmCompact.execute();
+			
+		} catch (SQLException | IOException e) {
+			logger.error(e.getMessage(), e);
+		}finally {
+			if(dbManager != null){
+				dbManager.disconnect();
+			}
+		}
+		
+    }
+}
---
+++ H2DBConn/src/main/java/com/hoffnungland/db/corner/h2dbconn/H2ConnectionManager.java
@@ -0,0 +1,15 @@
+package com.hoffnungland.db.corner.h2dbconn;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.hoffnungland.db.corner.dbconn.ConnectionManager;
+
+public class H2ConnectionManager extends ConnectionManager {
+	
+	private static final Logger logger = LogManager.getLogger(H2ConnectionManager.class);
+	
+	static {
+		ConnectionManager.registerDriver(new org.h2.Driver());
+	}
+}
